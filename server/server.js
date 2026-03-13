42838af234fb7f7484b9514d6be692699f373c0b
Aashi Bajpai <aashi.bajpai19@gmail.com>
2026-03-14 00:20:05 +0530

    Update server.js
    Connected hospital routes to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const hospitalRoutes = require("./routes/hospitalRoutes");
 app.use(express.json());
+app.use("/api", hospitalRoutes);
 app.get("/", (req, res) => {
   res.send("Hospital API running");
 });
